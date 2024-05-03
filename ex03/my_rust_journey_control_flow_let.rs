afc832b4114ee064aaea10e4d92cc606d38bea24
Mathius <info@yogimathius.dev>
2024-05-02 23:25:59 -0700

    feat: complete ex03

---
+++ ex03/my_rust_journey_control_flow_let.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+fn main() {
+    match env::args().nth(1) {
+        Some(argument) => {
+            let mut chars_iter = argument.chars();
+
+            while let Some(char) = chars_iter.next() {
+                println!("{}", char);
+            }
+        }
+        None => {
+            println!("Please provide an argument.");
+            return;
+        }
+    }
+}
