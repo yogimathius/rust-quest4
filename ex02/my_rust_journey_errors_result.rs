0a37c449aedd1f4a6a471b6675fb3158a8658277
Mathius <info@yogimathius.dev>
2024-05-02 23:13:49 -0700

    test: complete ex02?

--- ex02/my_rust_journey_errors_result.rs
+++ ex02/my_rust_journey_errors_result.rs
@@ -1,8 +1,44 @@
-let hawaii_volcanoes = ["Kilauea", "Maunaloa", "Maunakea", "Hualalai", "Kohala", "Leahi", "Oahu", "Haleakala"];
-let iceland_volcanoes = ["Fagradalsfjall", "Holuhraun", "Eyjafjallajokull", "Hekla", "Krafla", "Eldfell", "Reykjaneshryggur"];
-let usa_volcanoes = ["Lassen Peak", "Long Valley Caldera", "Yellowstone", "Mammoth Mountain", "Pinnacles", "Ubehebe Craters", "Mount Shasta", "Mount St. Helens", "Mount Rainier", "Mount Baker", "Mount Hood"];
+fn volcano_location(volcano: &str) -> Result<&str, &str> {
+    let hawaii_volcanoes = [
+        "Kilauea",
+        "Maunaloa",
+        "Maunakea",
+        "Hualalai",
+        "Kohala",
+        "Leahi",
+        "Oahu",
+        "Haleakala",
+    ];
+    let iceland_volcanoes = [
+        "Fagradalsfjall",
+        "Holuhraun",
+        "Eyjafjallajokull",
+        "Hekla",
+        "Krafla",
+        "Eldfell",
+        "Reykjaneshryggur",
+    ];
+    let usa_volcanoes = [
+        "Lassen Peak",
+        "Long Valley Caldera",
+        "Yellowstone",
+        "Mammoth Mountain",
+        "Pinnacles",
+        "Ubehebe Craters",
+        "Mount Shasta",
+        "Mount St. Helens",
+        "Mount Rainier",
+        "Mount Baker",
+        "Mount Hood",
+    ];
 
-fn volcano_location(volcano: &str) -> Result<&str, &str>
- {
-
- }
+    if hawaii_volcanoes.contains(&volcano) {
+        Ok("Hawaii")
+    } else if iceland_volcanoes.contains(&volcano) {
+        Ok("Iceland")
+    } else if usa_volcanoes.contains(&volcano) {
+        Ok("USA")
+    } else {
+        Err("Volcano not found")
+    }
+}
