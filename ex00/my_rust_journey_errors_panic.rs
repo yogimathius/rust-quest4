8827bb325d5143605b0ca2b216315ad701789f6c
mathius johnson <johnson_m@qwasar.io>
2024-05-03 06:09:25 +0000

    feat: complete ex00 for sure

--- ex00/my_rust_journey_errors_panic.rs
+++ ex00/my_rust_journey_errors_panic.rs
@@ -1,7 +1,8 @@
+use std::env;
 use std::panic;
 
 fn is_it_a_good_ide(ide: &str) {
-    let good_ide = ["vcode", "cloud9", "docode", "emacs", "nano"];
+    let good_ide = ["vscode", "cloud9", "docode", "emacs", "nano"];
     let not_good = ["vim", "atom"];
     let is_good = good_ide.contains(&ide);
     let is_not_good = not_good.contains(&ide);
@@ -9,7 +10,7 @@ fn is_it_a_good_ide(ide: &str) {
     match is_good {
         true => println!("Good"),
         false => match is_not_good {
-            true => println!("Not good."),
+            true => println!("Not good"),
             false => panic!("{} is not in the list", ide),
         },
     }
