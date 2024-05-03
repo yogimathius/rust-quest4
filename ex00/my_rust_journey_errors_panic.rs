80fdf01092f2d7deb87507753fe1a7e74edb0fa1
Mathius <info@yogimathius.dev>
2024-05-02 23:08:02 -0700

    feat: complete ex00

--- ex00/my_rust_journey_errors_panic.rs
+++ ex00/my_rust_journey_errors_panic.rs
@@ -1,7 +1,16 @@
+use std::panic;
+
 fn is_it_a_good_ide(ide: &str) {
-    if ide == "google doc" {
-        panic!("Not good.");
-    } else {
-        println!("Good");
+    let good_ide = ["vcode", "cloud9", "docode", "emacs", "nano"];
+    let not_good = ["vim", "atom"];
+    let is_good = good_ide.contains(&ide);
+    let is_not_good = not_good.contains(&ide);
+
+    match is_good {
+        true => println!("Good"),
+        false => match is_not_good {
+            true => println!("Not good."),
+            false => panic!("{} is not in the list", ide),
+        },
     }
 }
